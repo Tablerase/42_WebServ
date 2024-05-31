d3a16676cd13735fe55d0fe07d6061cb192f4050
Mal-back <purmerinos@protonmail.com>
2024-05-31 20:47:05 +0200

    Ugly code lol -- Rebuild the thing w/ a minimal epoll loop -- It is now possible to listen to two different port at once and return different index to each one of them

---
+++ test/srcs/PortListener.cpp
@@ -0,0 +1,160 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: purmerinos <purmerinos@protonmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/30 17:48:24 by purmerinos        #+#    #+#             */
+/*   Updated: 2024/05/30 17:48:24 by purmerinos       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <PortListener.hpp>
+#include <asm-generic/socket.h>
+#include <csignal>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+using namespace std;
+
+PortListener::PortListener(const char* port, const string& index) : _index(index) {
+	_portFd = socket(AF_INET, SOCK_STREAM, 0);
+	if (_portFd < 0) {
+		throw runtime_error(strerror(errno));
+		return;
+	}
+
+	struct addrinfo		hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	int	gai_error = getaddrinfo(NULL, port, &hints, &_res);
+
+	if (gai_error != 0) {
+		throw runtime_error(gai_strerror(gai_error));
+		return;
+	}
+	// This is only so kernel don't block the port for this socket only.
+	int reUse = 1;
+	setsockopt(_portFd, SOL_SOCKET, SO_REUSEADDR, &reUse, sizeof(reUse));
+
+	// Assign the socket to the address and port we define with the structure.
+	for (_address = _res; _address != NULL; _address = _address->ai_next) {
+		if (bind(_portFd, _address->ai_addr, _address->ai_addrlen) == 0) {
+			break;
+		}
+	}
+	if (_address == NULL) {
+		throw runtime_error(strerror(errno));
+		freeaddrinfo(_res);
+		_res = NULL;
+		close(_portFd);
+		return;
+	}
+
+	// Reserve the port and start listening to it.
+	if (listen(_portFd, 4096) < 0) {
+		throw runtime_error(strerror(errno));
+		close(_portFd);
+		return;
+	}
+}
+
+PortListener::~PortListener( void ) {
+	for (vector<int>::iterator it = _clientFds.begin(); it != _clientFds.end(); ++it) {
+		close(*it);
+	}
+	close(_portFd);
+	if (_res != NULL) {
+		freeaddrinfo(_res);
+	}
+}
+
+int	PortListener::getPortFd(void) const {
+	return(this->_portFd);
+}
+
+int PortListener::matchFd(int fd) const {
+	for (vector<int>::const_iterator it = _clientFds.begin();
+			it != _clientFds.end(); ++it) {
+		if (*it == fd) {
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int PortListener::hasPendingRequest( void ) const {
+	if (this->_request.size() != 0) {
+		return (1);
+	}
+	return (0);
+}
+
+int PortListener::getSocketFd( void ) {
+	cout << "I have been called lol" << endl;
+	const int socketFd = accept(this->_portFd, this->_address->ai_addr,
+			(socklen_t *)&this->_address->ai_addrlen);
+	if (socketFd < 0) {
+		throw runtime_error(strerror(errno));
+	}
+	_clientFds.push_back(socketFd);
+	return (socketFd);
+}
+
+void	PortListener::readRequest(int fd) {
+	char	buffer[4096];
+
+	int bytesRead = recv(fd, buffer, 4095, 0);
+	if (bytesRead == 0) {
+		cout << "Nothing to read" << endl;
+		close(fd);
+		vector<int>::iterator it = find(_clientFds.begin(), _clientFds.end(), fd); 
+		_clientFds.erase(it);
+		return ;
+	} else if (bytesRead < 0) {
+		throw runtime_error(strerror(errno));
+	}
+	this->_request += buffer;
+	std::cout << buffer << std::endl;
+}
+
+void PortListener::writeRequest(int fd) {
+	ifstream index_file;
+	index_file.open(this->_index.c_str());
+	if (index_file.fail()) {
+		throw runtime_error("Failed to open file");
+	}
+	index_file.seekg(0, index_file.end);
+	int file_size = index_file.tellg();
+	index_file.seekg(0, index_file.beg);
+	_response = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\nContent-Length: ";
+	char size[12];
+	sprintf(size, "%d", file_size);
+	_response += size;
+	_response += "\r\n\r\n";
+	ostringstream sstream;
+	sstream << index_file.rdbuf();
+	_response += sstream.str();
+	cout << _response << endl;
+	write(fd, _response.c_str(), _response.size());
+	_response.clear();
+	_request.clear();
+}
---
+++ test/srcs/main.cpp
@@ -0,0 +1,171 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simple_socket.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: purmerinos <purmerinos@protonmail.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 15:53:08 by purmerinos        #+#    #+#             */
+/*   Updated: 2024/05/29 15:53:08 by purmerinos       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <asm-generic/socket.h>
+#include <cctype>
+#include <csignal>
+#include <ios>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string>
+#include <PortListener.hpp>
+
+using namespace std;
+
+int server_is_running;
+
+void	send_index(int new_request);
+void sig_handler(int signal);
+
+// TO use me : Compile this and launch it. then open a web browser and type : 127.0.0.1:8080 on a web browser.
+// The terminal where the server is launch should print the content of the request and web browser should display content of 
+// index.html.
+
+int add_event(int epollfd, PortListener* host) {
+	struct epoll_event new_client;
+	new_client.events = EPOLLIN | EPOLLOUT;
+	new_client.data.fd = host->getSocketFd();
+	return (epoll_ctl(epollfd, EPOLL_CTL_ADD, new_client.data.fd, &new_client));
+}
+
+PortListener* getGoodListener(PortListener *port1, PortListener *port2, int fd) {
+	if (port1->matchFd(fd) == 1) {
+		return (port1);
+	}
+	return (port2);
+}
+
+int main() {
+
+	// First we create a socket through a syscall.
+
+	PortListener* port1;
+	PortListener* port2;
+
+	try {
+		port1 = new PortListener("1312", "jul.html");
+	} catch (runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+	try {
+		port2 = new PortListener("1313", "sch.html");
+	} catch (runtime_error& e) {
+		delete port1;
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+	int epollfd = epoll_create(1);
+	if (epollfd < 1) {
+		perror("Epoll error : ");
+		delete port1;
+		delete port2;
+	}
+	struct epoll_event portfds[2];
+	portfds[0].data.fd = port1->getPortFd();
+	portfds[0].events = EPOLLIN;
+	portfds[1].data.fd = port2->getPortFd();
+	portfds[1].events = EPOLLIN;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, port1->getPortFd(), &portfds[0]) < 0) {
+		delete port1;
+		delete port2;
+		close (epollfd);
+		perror("epoll ctl");
+	}
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, port2->getPortFd(), &portfds[1]) < 0) {
+		delete port1;
+		delete port2;
+		close (epollfd);
+		perror("epoll ctl");
+	}
+	server_is_running = 1;
+	signal(SIGINT, sig_handler);
+	signal(SIGPIPE, SIG_IGN);
+	struct epoll_event receiver;
+	memset(&receiver, 0, sizeof(receiver));
+	cout << "port 1 " << port1->getPortFd() << "port 2 " << port2->getPortFd() << endl;
+	while (server_is_running) {
+		// std::cout << "Hey !" << std::endl;
+		int	events = epoll_wait(epollfd, &receiver, 1, 500);
+		if (events < 0) {
+			perror("Epoll failed");
+			break;
+		}
+		std::cout << "received events : " << events << std::endl;
+		if (events == 0) {
+			continue;
+			std::cout << "Port1 fd : " << port1->getPortFd() << std::endl;
+			std::cout << "Port2 fd : " << port2->getPortFd() << std::endl;
+			std::cout << "Events Reported fd : " << receiver.data.fd << std::endl;
+		}
+		if (receiver.data.fd == port1->getPortFd()) {
+			std::cout << "Got ya !" << std::endl;
+			try {
+				if (add_event(epollfd, port1) < 0) {
+					perror("epoll ctl");
+					break ;
+				}
+			} catch (runtime_error& e) {
+				std::cerr << e.what() << std::endl;
+				break ;
+			}
+		}
+		else if (receiver.data.fd == port2->getPortFd()) {
+			try {
+				if (add_event(epollfd, port2) < 0) {
+					perror("epoll ctl");
+					break;
+				}
+			} catch (runtime_error& e) {
+				std::cerr << e.what() << std::endl;
+				break ;
+			}
+		}
+		else {
+		cout << "Receiver fd : " << receiver.data.fd << "Events : CHeck for epollin" << (receiver.events & EPOLLIN) << "Check for EPOLLOUT " << (receiver.events & EPOLLIN) << endl;
+			PortListener * const of_interest = getGoodListener(port1, port2, receiver.data.fd);
+			cout << of_interest->hasPendingRequest() << endl;
+			if ((receiver.events & EPOLLOUT) && of_interest->hasPendingRequest() == 1) {
+				of_interest->writeRequest(receiver.data.fd);
+			} 
+			else if ((receiver.events & EPOLLIN) && of_interest->hasPendingRequest() == 0) {
+				try {
+					of_interest->readRequest(receiver.data.fd);
+				} catch (runtime_error& e) {
+					std::cerr << e.what() << std::endl;
+					break ;
+				}
+			}
+		}
+	}
+	delete port1;
+	delete port2;
+	close (epollfd);
+	return (0);
+}
+
+void sig_handler(int signal) {
+	(void)signal;
+	server_is_running = 0;
+	return;
+}
