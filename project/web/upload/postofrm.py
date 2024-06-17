ca7741704204de226f7f2046f4d825d353c99b21
Mal-back <purmerinos@protonmail.com>
2024-06-17 06:30:10 +0200

    CGI post seems to work lol i'm going to sleep now

---
+++ project/web/upload/postofrm.py
@@ -0,0 +1,26 @@
+#!usr/bin/python3
+
+import cgi, cgitb
+
+form = cgi.FieldStorage()
+
+username = form["username"].value
+emailaddress = form["emailaddress"].value
+
+print("Content-type:text/html")
+
+content = ("<html>")
+content += ("<head>")
+content += ("<title> MY FIRST CGI FILE </title>")
+content += ("</head>")
+content += ("<body>")
+content += ("<h3> This is HTML's Body Section </h3>")
+content += ("UserName is : ")
+content += (username)
+content += ("\nEmail is : ")
+content += (emailaddress)
+content += ("</body>")
+content += ("</html>")
+
+print(f"Content-Length: {len(content)}\r\n\r\n")
+print(f"{content}")
